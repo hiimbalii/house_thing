42040df3e9046227731fbd3c8972ef04d8050660
Daniel Toth <tcrbt1@gmail.com>
2025-03-15 16:02:30 +0100

    fixes chakra icons

--- app/src/components/main/back.tsx
+++ app/src/components/main/back.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { Box, Flex, Button } from "@chakra-ui/react";
-import { ChevronLeftIcon } from "@chakra-ui/icons";
 import Link from "next/link";
 
 export const BackButtonWrapper = ({
@@ -12,9 +11,7 @@ export const BackButtonWrapper = ({
     <Box>
       <Flex flexDir="column" gap={4}>
         <Button variant="ghost" asChild>
-          <Link href="/">
-            <ChevronLeftIcon /> Back
-          </Link>
+          <Link href="/">{"< Back"}</Link>
         </Button>
         <Box>{children}</Box>
       </Flex>
